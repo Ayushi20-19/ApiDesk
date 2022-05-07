9a8bda2f7b134223413e7e9c209603c4f3b9c749
Asuraking1n <nishant99tiwari@gmail.com>
2022-05-07 09:40:47 -0700

    UI change

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className='App'>
     <div className="routes-cont">
-      <div className="route" onClick={()=>setIsStackOverflow(false) } style={{ backgroundColor:istackOverflow ? '#fff':'#3f6e3f', color:!istackOverflow ? '#fff':'#3f6e3f'}}>API</div>
-      <div className="route sec-route"  onClick={()=>setIsStackOverflow(true)} style={{ backgroundColor:!istackOverflow ? '#fff':'#3f6e3f', color:istackOverflow ? '#fff':'#3f6e3f'}}>StackOverflow</div>
+      <div className="route" onClick={()=>setIsStackOverflow(false) } style={{ backgroundColor:istackOverflow ? '#fff':'#189AB4', color:!istackOverflow ? '#fff':'#189AB4'}}>API</div>
+      <div className="route sec-route"  onClick={()=>setIsStackOverflow(true)} style={{ backgroundColor:!istackOverflow ? '#fff':'#189AB4', color:istackOverflow ? '#fff':'#189AB4'}}>StackOverflow</div>
     </div>
     {istackOverflow ?<StackOverflow/>:<ShowApiData />}
     </div>
